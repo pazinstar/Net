01670eff525252f4e3a6a329ed14ad33cdeffb6b
Kelvin Mukaria <145207479+pazinstar@users.noreply.github.com>
2025-03-03 02:16:33 -0500

    Update settings.py

--- simple_balance_app/settings.py
+++ simple_balance_app/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 from pathlib import Path
 
