45b8b38082429e7379b79e233d8250fea316597f
Kelvin Mukaria <145207479+pazinstar@users.noreply.github.com>
2025-03-03 02:21:04 -0500

    Update settings.py

--- simple_balance_app/settings.py
+++ simple_balance_app/settings.py
@@ -11,14 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
